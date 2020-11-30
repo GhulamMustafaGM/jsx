0401fa000b9f173a42f73d6fdfa706bc611a13eb
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-30 16:05:27 +0100

    worked on react components

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component 
 
-
+const App = () => {
+    return <div> Hi there! </div>;
+};
 
 // Take the react component and show it on the screen 
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
